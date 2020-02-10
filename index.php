eed25401b1f9ac17dcb458d0de4539e6ad08eba9
dylan <dylanspin100@hotmail.com>
2020-02-10 09:30:14 +0100

    Start Project
    Standaard dingen

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Uplink main page"/>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <script src="javascript/javascript.js"></script>
+    <title>Uplink</title>
+  </head>
+  <body>
+
+    <div class="header">
+
+    </div>
+
+  </body>
+</html>
