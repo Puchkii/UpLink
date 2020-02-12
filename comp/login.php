79cfc241cb7d56ce47d398f6ffe2d0029012dada
dylan <dylanspin100@hotmail.com>
2020-02-12 09:45:35 +0100

    Header/login Update
    Wat aan passingen aan de header moet nog meer later denkt maar nu hou ik het simple en er is een login page

---
+++ comp/login.php
@@ -0,0 +1,17 @@
+
+
+<form class="login" method="post">
+  <div class="loginKop">
+    Login
+  </div>
+  <div class="loginInputLoc">
+    <h4>Username</h4>
+    <input type="text" name="username" class="inlog_input" placeholder="Username" autocomplete="off">
+
+    <h4>Password</h4>
+    <input type="password" name="password" class="inlog_input" placeholder="Password">
+
+    <input type="submit" name="login" value="Login" class="loginButton">
+    <a href="#" class="login_dont underline">Dont have a Account ?</a>
+  </div>
+</form>
--- comp/navbar.php
+++ comp/navbar.php
@@ -3,7 +3,7 @@
   if(!isset($_COOKIE['cookies']))//als cookie niet gezet is show cookie melding
   {
  ?>
- 
+
   <div class="cookieMelding" id="Cookies">
     <div class="cookieText">
       To use our site to the max potential we need you to accept our cookies.
@@ -18,15 +18,18 @@
 <form class="header">
   <a href="index.php" class="logo">Upl!nk</a><!--Logo-->
 
-  <a href="" class="headerButton">Ergens</a><!--Header buttons-->
-  <a href="" class="headerButton">Ergens</a>
-  <a href="" class="headerButton">Ergens</a>
-  <a href="" class="headerButton">Ergens</a>
+  <a href="" class="headerButton">Problems</a><!--Header buttons-->
+  <a href="" class="headerButton">Tips</a>
+  <a href="" class="headerButton">Friends</a>
+  <a href="" class="headerButton">Profile</a>
 
   <input type="text" name="search" placeholder="Search..." class="SearchBar" autocomplete="off"><!--Search bar-->
 
   <div class="loginbuttonLoc"><!--Login/register buttons-->
-    <button href="#" class="inlogButton">Login</button>
-    <button href="#" class="inlogButton">Join Now</button>
+    <a href="login.php" class="inlogButton">Login</a>
+    <a href="register.php" class="inlogButton">Join Now</a>
+  </div>
+  <div class="mobileMenu"><!--Mobile menu-->
+    <i class="fa fa-bars" aria-hidden="true"></i>
   </div>
 </form>
