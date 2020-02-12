4d63eba8c48a9a5f716c9ba8b1b7530015160c68
dylan <dylanspin100@hotmail.com>
2020-02-12 10:52:40 +0100

    Update main page
    heb 2 dingen toegevoegd aan de main page

--- comp/intro.php
+++ comp/intro.php
@@ -1,4 +1,16 @@
 
 <div class="intro">
+  
+  <div class="introText">
+      Stock text random text over Upl!nk
+      nog wat random text
+  </div>
+  <div class="introText2">
+      iaculis nisl nunc quis ligula. Donec mollis quis lacus et mollis.
+      Nulla ante odio, fermentum pellentesque condimentum convallis, hendrerit vitae elit.
+  </div>
 
+  <div class="introImage">
+    <img src="img/stock1.jpg" alt="Stock image" class="image">
+  </div>
 </div>
--- comp/main.php
+++ comp/main.php
@@ -1,4 +1,45 @@
 
 <div class="main">
 
+  <div class="mainKop">
+    Latest news from Upl!nk
+  </div>
+
+  <div class="mainNews"><!--dit moet later via php dat je in de backend new news dingen kan toevoegen-->
+    <div class="newsCato">
+      <i class="fa fa-handshake-o" aria-hidden="true"></i>
+    </div>
+    <div class="newsKop">
+        New Partnership
+    </div>
+    <div class="newsText">
+        ac eget nibh. Fusce non hendrerit arcu. Curabitur justo nibh, sagittis eget
+        pharetra vel, suscipit vel nisl. Pellentesque quis gravida nunc.
+    </div>
+  </div>
+  <div class="mainNews">
+    <div class="newsCato">
+      <i class="fa fa-wrench" aria-hidden="true"></i>
+    </div>
+    <div class="newsKop">
+        Some new Bug fixes
+    </div>
+    <div class="newsText">
+        ac eget nibh. Fusce non hendrerit arcu. Curabitur justo nibh, sagittis eget
+        pharetra vel, suscipit vel nisl. Pellentesque quis gravida nunc.
+    </div>
+  </div>
+  <div class="mainNews mainNews2">
+    <div class="newsCato">
+      <i class="fa fa-plus" aria-hidden="true"></i>
+    </div>
+    <div class="newsKop">
+      Added a new features
+    </div>
+    <div class="newsText">
+        ac eget nibh. Fusce non hendrerit arcu. Curabitur justo nibh, sagittis eget
+        pharetra vel, suscipit vel nisl. Pellentesque quis gravida nunc.
+    </div>
+  </div>
+
 </div>
