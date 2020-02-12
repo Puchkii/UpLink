a2af2c24c9710e1c8a5b62f1eb65b6904766b1df
Kevin Blank <kevinblank72@gmail.com>
2020-02-12 23:09:18 +0100

    navbar ziet er wat beter uit. Er zitten nog wel designflaws in.

--- comp/navbar.php
+++ comp/navbar.php
@@ -9,41 +9,40 @@
       To use our site to the max potential we need you to accept our cookies.
     </div>
     <button class="cookieAccept" onclick="cookieClose()">
-      I Accept
+      I Accept :)
     </button>
   </div>
 
 <?php } ?>
 <!-- Bootstrap navbar. Er moet nog een  -->
-<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-  <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a href="./index.php" class="navbar-brand"><img src="./img/logo.jpg" style=" width:25%;">UpL!nk</a> 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
   <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarResponsive">
-    <ul class="navbar-nav ml-auto">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a href="#" class="">Problems</a>
+        <a href="#" class="inlogButton nav-link">Problems</a>
       </li>
       <li class="nav-item active">
-        <a href="#" class="">CheatSheet</a>
+        <a href="#" class="inlogButton nav-link">CheatSheet</a>
       </li>
       <li class="nav-item active">
-        <a href="#" class="">Friends</a>
+        <a href="#" class="inlogButton nav-link">Friends</a>
       </li>
       <li class="nav-item active">
-        <a href="#" class="">Profile</a>
+        <a href="#" class="inlogButton nav-link">Profile</a>
       </li>
       <li>
-      <input type="text" name="search" placeholder="Search..." class="SearchBar" autocomplete="off"><!--Search bar-->
+      <input type="text" name="search" placeholder="Search..." class="inlogButton nav-link" autocomplete="off">
       </li>
       <li class="nav-item active">
-        <a href="login.php" class="inlogButton">Login</a>
+        <a href="login.php" class="inlogButton nav-link">Log in</a>
       </li>
       <li class="nav-item active">
-        <a href="register.php" class="inlogButton">Join Now</a>
+        <a href="register.php" class="inlogButton nav-link">Sign up</a>
       </li>
     </ul>
 
