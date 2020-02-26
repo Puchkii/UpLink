1d82749db81363aed49d559c2b552e496d114f0c
Kevin Blank <kevinblank72@gmail.com>
2020-02-26 23:53:14 +0100

    added some bootstrap to register

--- comp/register.php
+++ comp/register.php
@@ -1,9 +1,23 @@
-
-<form class="" method="post">
-  <input type="text" name="username" placeholder="Username">
-  <input type="email" name="email" placeholder="Email">
-  <input type="date" name="birthDate">
-  <input type="password" name="password" placeholder="Password">
-  <input type="password" name="passwordRepeat" placeholder="Repeat">
-  <input type="submit" name="submitReg" value="Submit">
-</form>
+<div class="container text-center">
+  <form method="post">
+    <div class="form-group">
+      <label for="formGroupExampleInput"><h4>Username</h4></label>
+      <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter username">
+    </div>
+    <div class="form-group">
+      <label for="formGroupExampleInput"><h4>Email</h4></label>
+      <input type="email" name="email" placeholder="Email">
+    </div>
+    <div class="form-group">
+      <label for="formGroupExampleInput"><h4>Birthdate</h4></label>
+      <input type="date" name="birthDate">
+    </div>
+    <div class="form-group">
+      <label for="formGroupExampleInput2"><h4>Password</h4></label>
+      <input type="password" class="form-control" id="formGroupExampleInput2" placeholder="Enter password">
+      <input type="password" name="form-control passwordRepeat" placeholder="Repeat password">
+    </div>
+      <input type="submit" name="submitReg" value="Submit">
+      <button type="submit" name="register" value="Submit" class="btn btn-outline-secondary">Register now</button>
+  </form>
+</div>
