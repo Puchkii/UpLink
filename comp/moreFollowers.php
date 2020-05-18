939d7a6e9e7372d0e8b5ed47a580726b47d7ea0b
Kevin Blank <kevinblank72@gmail.com>
2020-05-18 09:59:26 +0200

    kaartje voor allfollowers

--- comp/moreFollowers.php
+++ comp/moreFollowers.php
@@ -1,6 +1,13 @@
+<div class='row'><div class='col-sm-4'><div class='card' style='width: 18rem;'>
+<img src='img/profileImage/$profileFOL' class='card-img-top' alt='Profile Image'>
+<div class='card-body'>
+  <h5 class='card-title'>$aboutFOL</h5>
+  <button typ='submit' value='$i' class='btn btn-outline-secondary' name='unfollow'>Unfollow</button>
+</div></div></div></div><br>
+
 <?php
     //kevin dit moet nog in een table met vakjes waar een image(profileImage) instaat met de naam er naast
     for($i=0; $i<Count($followersProfileBE); $i++){
-      echo "<button name='bezoek' type='submit' value='$followersProfileBE[$i]' onclick='resetCookie()'>".$followersProfileBE[$i]."</button><br>";//zo dat je ze ook kan bezoeken
+      echo "<button name='bezoek' class='btn btn-outline-secondary' type='submit' value='$followersProfileBE[$i]' onclick='resetCookie()'>".$followersProfileBE[$i]."</button><br><br>";//zo dat je ze ook kan bezoeken
     }
 ?>
